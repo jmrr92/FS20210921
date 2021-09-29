7e6724463fbbee3170d02e8072e9b505396bd256
jmrr92 <jesusmanuelrodriguezruiz@gmail.com>
2021-09-29 15:31:27 +0200

    cambios spec

--- fundamentos/spec/ejercicios-js.spec.js
+++ fundamentos/spec/ejercicios-js.spec.js
@@ -1,17 +1,24 @@
 describe("Pruebas ejercicios", function () {
     describe("Aleatorio", function () {
         it('Aleatorio 0 - 100', function(){
-            let min;
-            let max;
+            let min = prompt("Escribe un numero");
+            let max = prompt("Escribe un numero");
 
-            if (min >=0 && max <100){
-                expect(true).toBeTruthy()
-            }
-            else expect(false).not.toBeTruthy()
+            expect(min).toBeGreaterThanOrEqual(0);
+            expect(max).toBeLessThanOrEqual(100);
         })
     })
 
-    describe("Palindromo", function(){
-        it('')
+    describe("Juego", function () {
+        it('Generado', function(){
+            let generado = Math.floor(Math.random() * 100) + 1;
+
+            expect(generado).toBeGreaterThanOrEqual(0);
+            expect(generado).toBeLessThanOrEqual(100);
+
+            let numAdivinar = prompt("Escribe un numero:")
+
+            expect(generado).toEqual(numAdivinar);
+        })
     })
 })
