dcd7a48a3b9fde3b6b94f9f92c49e77689582d13
Jesus <jesusmanuelrodriguezruiz@gmail.com>
2021-10-05 23:25:49 +0200

    pruebas calculadora

--- angular/src/app/calculadora/calculadora.component.ts
+++ angular/src/app/calculadora/calculadora.component.ts
@@ -3,13 +3,83 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.scss']
+  styleUrls: ['./calculadora.component.scss'],
 })
 export class CalculadoraComponent implements OnInit {
+  pantalla: string;
+  acumulado: number;
+  op: string;
+  resultado: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.pantalla = '0';
+    this.acumulado = 0;
+    this.op = '+';
+    this.resultado = true;
   }
 
+  ponerDigito(num: string) {
+    if (this.pantalla == '0' || this.resultado) {
+      this.pantalla = num;
+      this.resultado = false;
+    } else {
+      this.pantalla += num;
+    }
+  }
+
+  operar(operador: string) {
+    let valor = parseFloat(this.pantalla);
+    switch (this.op) {
+      case '+':
+        this.acumulado += valor;
+        break;
+      case '-':
+        this.acumulado -= valor;
+        break;
+      case '*':
+        this.acumulado *= valor;
+        break;
+      case '/':
+        this.acumulado /= valor;
+        break;
+    }
+
+    this.op = operador;
+    this.resultado = true;
+    this.pantalla = this.acumulado.toString();
+  }
+
+  borrarDigito() {
+    if (
+      this.resultado ||
+      this.pantalla.length == 1 ||
+      (this.pantalla.length == 2 && this.pantalla.startsWith('-'))
+    ) {
+      this.pantalla = '0';
+      this.resultado = true;
+    } else this.pantalla = this.pantalla.substr(0, this.pantalla.length - 1);
+  }
+
+  borrarTodo() {
+    this.acumulado = 0;
+    this.op = '+';
+    this.pantalla = '0';
+    this.resultado = true;
+  }
+
+  cambiarSigno() {
+    this.pantalla = (-this.pantalla).toString();
+    this.acumulado = -this.acumulado;
+  }
+
+  ponerComa() {
+    if (this.resultado) {
+      this.pantalla = '0.';
+      this.resultado = false;
+    } else {
+      this.pantalla += '.';
+    }
+  }
+
+  ngOnInit(): void {}
 }
