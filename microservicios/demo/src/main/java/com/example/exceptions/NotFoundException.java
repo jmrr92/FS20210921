edb9d519400c6090c42690bf87ba16a606fb45c0
jmrr92 <jesusmanuelrodriguezruiz@gmail.com>
2021-10-21 12:24:27 +0200

    added notfoundexception

---
+++ microservicios/demo/src/main/java/com/example/exceptions/NotFoundException.java
@@ -0,0 +1,28 @@
+package com.example.exceptions;
+
+public class NotFoundException extends Exception {
+
+	private static final long serialVersionUID = 8906380427954644599L;
+	final static String MESSAGE_STRING = "Not found";
+
+	public NotFoundException() {
+		super(MESSAGE_STRING);
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+	public NotFoundException(Throwable cause) {
+		super(MESSAGE_STRING);
+	}
+
+	public NotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
