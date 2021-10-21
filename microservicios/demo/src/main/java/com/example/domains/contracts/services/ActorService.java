e921b9824b9bb57322cd7298409ec699182cf896
jmrr92 <jesusmanuelrodriguezruiz@gmail.com>
2021-10-21 12:21:46 +0200

    exceptions

---
+++ microservicios/demo/src/main/java/com/example/domains/contracts/services/ActorService.java
@@ -0,0 +1,8 @@
+package com.example.domains.contracts.services;
+
+import com.example.domains.entities.Actor;
+import com.example.domains.entities.core.services.contracts.DomainService;
+
+public interface ActorService extends DomainService<Actor, Integer>{
+
+}
---
+++ microservicios/demo/src/main/java/com/example/exceptions/InvalidDataException.java
@@ -0,0 +1,29 @@
+package com.example.exceptions;
+
+public class InvalidDataException extends Exception {
+
+	private static final long serialVersionUID = 8906380427954644599L;
+	final static String MESSAGE_STRING = "Invalid data";
+
+	public InvalidDataException() {
+		super(MESSAGE_STRING);
+	}
+
+	public InvalidDataException(String message) {
+		super(message);
+	}
+
+	public InvalidDataException(Throwable cause) {
+		super(MESSAGE_STRING);
+	}
+
+	public InvalidDataException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidDataException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
